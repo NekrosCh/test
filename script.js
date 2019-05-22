53c7cfda3c6748f307d070b11815b8292c6bfeb1
Nekros <elvhant@gmail.com>
2019-05-22 23:53:41 +0300

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,44 @@
+'use strict'
+
+
+
+/*console.log(second);
+var leftBorderWidth = 1;
+let second = 2;
+const pi =3.14;
+console.log(second); */
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log("string"*9);
+
+let something;
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+
+}
+
+console.log(persone["name"]);
+let arr = ["plum.png",'orange/jpg', 'apple.bmp'];
+console.log(arr[0]);
+
+//alert("Hello World!");
+// let answer = confirm("Are you here?");
+// console.log(answer);
+// let answer = +prompt("Есть ли вам 18 лет?", "Да");
+// console.log(typeof(answer));
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
