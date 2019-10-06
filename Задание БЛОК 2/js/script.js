51e6512b8774d27c467c4721626c9f9dcbe2e300
Nekros <Elvhant@gmail.com>
2019-10-06 19:24:55 +0300

    14

--- Задание БЛОК 2/js/script.js
+++ Задание БЛОК 2/js/script.js
@@ -225,46 +225,65 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // Рабочий вариант из комментариев в обычном формате;
 
-let message = {
+    let message = {
             loading: 'Загрузка...',
             success: 'Спасибо! Скоро свяжемся с вами!',
             failure: 'Что-то пошло не так...'
         };
  
-        let form = document.getElementsByTagName('form'),
+        let form = document.getElementsByClassName('main-form')[0],
+            formBottom =document.getElementById('form'),
             input = document.getElementsByTagName('input'),
             statusMessage = document.createElement('div');
- 
             statusMessage.classList.add('status');
  
-    for (let i = 0; i < form.length; i++) {
-        form[i].addEventListener('submit', function(event) {
+    function sendForm(elem){
+
+        elem.addEventListener('submit', function(event) {
             event.preventDefault();
-            form[i].appendChild(statusMessage);
- 
+            elem.appendChild(statusMessage);
+            let formData = new FormData(elem);
+            
+            function postData (data) {
+            return new Promise(function(resolve,reject) {
             let request = new XMLHttpRequest();
             request.open('POST', 'server.php');
             request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
  
-            let formData = new FormData(form[i]);
-            request.send(formData);
  
             request.addEventListener('readystatechange', function() {
                 if (request.readyState < 4) {
-                    statusMessage.innerHTML = message.loading;
+                   resolve();
                 } else if (request.readyState === 4 && request.status == 200) {
-                    statusMessage.innerHTML = message.success;
+                    resolve();
                 } else {
-                    statusMessage.innerHTML = message.failure;
+                    reject();
                 }
             });
  
+            request.send(data);
+        
+            });
+            }
+        
+
+        function clearInput () {
             for (let i = 0; i < input.length; i++) {
                 input[i].value = '';
             }
-        });
-    }
+        }
+
+        postData(formData)
+            .then(()=> statusMessage.innerHTML = message.loading)
+            .then(()=> statusMessage.innerHTML = message.success)
+            .catch(()=> statusMessage.innerHTML = message.failure)
+            .then(clearInput);
+            
+    
+    });
+}
+
+sendForm(form);
+sendForm(formBottom);
      
 });
-
-
